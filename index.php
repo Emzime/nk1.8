ed4e281552a64529f58c006397019411a9410f22
Maxxi <nk.maxxi@gmail.com>
2013-02-04 15:41:34 +0100

    Update index.php
    remove nkSiteClosed and Add include('Includes/nkSiteClosed');

--- index.php
+++ index.php
@@ -118,47 +118,8 @@ else $visiteur = $user[1];
 translate('lang/' . $language . '.lang.php');
 
 if ($nuked['nk_status'] == 'closed' && $user[1] < 9 && $_REQUEST['op'] != 'login_screen' && $_REQUEST['op'] != 'login_message' && $_REQUEST['op'] != 'login'){
-?>
-    <!DOCTYPE html>
-    <html lang="<?php echo $lang; ?>">
-        <head>
-            <title><?php echo $nuked['name']; ?>&nbsp;-&nbsp;<?php echo $nuked['slogan']; ?></title>
-            <meta charset="utf-8" />
-            <link title="style" type="text/css" rel="stylesheet" href="css/nkCss.css" />
-        </head>
-        <body id="nkSiteClose">
-            <section>
-                <header>
-                    <hgroup>
-                        <img src="images/logo.png" />
-                        <h1><?php echo $nuked['name']; ?></h1>
-                        <h2><?php echo $nuked['slogan']; ?></h2>
-                    </hgroup>
-                </header>
-                <article>
-                    <p><?php echo SITECLOSED; ?></p>
-                    <form action="index.php?file=User&amp;nuked_nude=index&amp;op=login" method="post">
-                        <div>
-                            <label for="pseudo"><?php echo PSEUDO; ?></label>
-                                <input id="pseudo" type="text" name="pseudo" size="15" maxlength="180" />
-                         </div>
-                        <div>
-                            <label for="password"><?php echo PASSWORD; ?></label>
-                                <input type="password" id="password" name="pass" size="15" maxlength="15" />                        
-                                <input type="hidden" class="checkbox" name="remember_me" value="ok" checked="checked" />
-                        </div>
-                                <input type="submit" value="<?php echo TOLOG; ?>" />      
-                    </form>         
-                </article>
-                <footer>
-                    <p>
-                        <a href="/"><?php echo $nuked['name']; ?></a> &copy; 2001, <?php echo date(Y); ?>&nbsp;|&nbsp;<?php echo POWERED; ?> <a href="http://www.nuked-klan.org">Nuked-Klan</a>
-                    </p>
-                </footer>
-            </section>
-        </body>
-    </html>
-<?php
+  /* Add page for website close */
+  include ('Includes/nkSiteClosed.php');
 }else if (($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin' || (isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == 'admin')) && $_SESSION['admin'] == 0){
     include('modules/Admin/login.php');
 }else if (($_REQUEST['file'] != 'Admin' AND $_REQUEST['page'] != 'admin') || ( nivo_mod($_REQUEST['file']) === false || (nivo_mod($_REQUEST['file']) > -1 && (nivo_mod($_REQUEST['file']) <= $visiteur))) ){

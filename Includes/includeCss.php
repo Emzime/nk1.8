efc831eea1f4129bb3f5f39eaebab31c0d49ddb5
Maxxi <nk.maxxi@gmail.com>
2013-02-06 00:53:19 +0100

    Update includeCss.css and NK_functions.php

--- Includes/includeCss.php
+++ Includes/includeCss.php
@@ -8,3 +8,6 @@
 
     <!-- Css de JQuery UI lastest -->
     <link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/ui-lightness/jquery-ui.css" media="screen" />
+
+    <!-- Css pour tooltip -->
+    <link type="text/css" rel="stylesheet" href="media/css/nkTooltipSter.css" media="screen" />

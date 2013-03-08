7deaf60b24e7455002ef4316ccc5ab20087abdad
Maxxi <nk.maxxi@gmail.com>
2013-03-08 19:05:38 +0100

    remove unnecessary style

--- Includes/nkCaptcha.php
+++ Includes/nkCaptcha.php
@@ -13,9 +13,9 @@ if (!defined("INDEX_CHECK")) exit('You can\'t run this file alone.');
 //réglage captcha (auto | on | off)
 define("NKCAPTCHA","auto");
 if (isset($_SESSION['captcha'])){
-	$GLOBALS['nkCaptchaCache'] = $_SESSION['captcha'];
+    $GLOBALS['nkCaptchaCache'] = $_SESSION['captcha'];
 }else{
-	$GLOBALS['nkCaptchaCache'] = uniqid();
+    $GLOBALS['nkCaptchaCache'] = uniqid();
 }
 
 require_once (dirname(__FILE__) . '/hash.php');
@@ -25,12 +25,12 @@ require_once (dirname(__FILE__) . '/hash.php');
 * @return string Generated code
 **/
 function Captcha_Generator(){
-	global $cookie_captcha;
-	static $code = null;
-	if ($code == null)
-		$code = substr(md5(uniqid()), rand(0, 25), 5);
-	$_SESSION['captcha'] = $code;
-	return $code;
+    global $cookie_captcha;
+    static $code = null;
+    if ($code == null)
+        $code = substr(md5(uniqid()), rand(0, 25), 5);
+    $_SESSION['captcha'] = $code;
+    return $code;
 }
 
 /**
@@ -38,72 +38,38 @@ function Captcha_Generator(){
 * @param string $code
 * @return bool
 **/
-function ValidCaptchaCode($code){
-	global $user;
-	return NKCAPTCHA == 'off' || ($user != null && $user[1] > 0) || strtolower($GLOBALS['nkCaptchaCache']) == strtolower($code);
+function ValidCaptchaCode($code) {
+    global $user;
+
+    return NKCAPTCHA == 'off' || ($user != null && $user[1] > 0) || strtolower($GLOBALS['nkCaptchaCache']) == strtolower($code);
 }
 
-function create_captcha($style){
+function create_captcha() {
+    global $nuked;
     $random_code = Captcha_Generator();
-
-    if ($style == 1){
     ?>
-
-		<p>&nbsp;<?php echo SECURITYCODE; ?>&nbsp;: 
-
-		<?php
-		if (@extension_loaded('gd')){
-		?>		
-			&nbsp;<img src="captcha.php" alt="" title="<?php echo SECURITYCODE; ?>" /></p>
-		<?php
-		}else {
-		?>
-			&nbsp;<big><i><?php echo $random_code; ?></i></big></p>
-		<?php
-		}
-		?>
-
-		<label for="code"><?php echo TYPESECCODE; ?></label>&nbsp;:&nbsp;
-			<input type="text" id="code" name="code_confirm" size="7" maxlength="5" />
-
-	<?php	
-    }
-    else if ($style == 2){
-    ?>
-		<p>&nbsp;<?php echo SECURITYCODE; ?>&nbsp;:</p>
-
-		<?php
-		if (@extension_loaded('gd')){
-		?>
-
-			<img src="captcha.php" alt="" title="' , SECURITYCODE , '" />
-
-		<?php
-		}else{
-		?>
-		 	<?php echo $random_code; ?>
-
-			<label for="code"><?php echo TYPESECCODE; ?></label>&nbsp;:&nbsp;
-				<input type="text" id="code" name="code_confirm" size="6" maxlength="5" />
-	<?php	
-		}
-    }else{
-
-		if (@extension_loaded('gd')){
-		?>
-
-			<img src="captcha.php" alt="" title="<?php echo SECURITYCODE; ?>" />
-
-		<?php
-		}else{
-		?>
-			<?php echo $random_code; ?>
-			<label for="code"><?php echo TYPESECCODE; ?></label>&nbsp;:&nbsp;
-				<input type="text" name="code_confirm" id="code" size="7" maxlength="5" />
-		<?php
-    	}
-    }
-	return($random_code);
+    <div id="captcha">
+        <div id="captchaImg">
+            <label class="nkLabelSpacing"><?php echo SECURITYCODE; ?></label>&nbsp;: &nbsp;
+            <?php
+            if (@extension_loaded('gd')) {
+            ?>      
+                <img src="captcha.php" alt="" title="<?php echo SECURITYCODE; ?>" />
+            <?php
+            } else {
+            ?>
+                <?php echo $random_code; ?>
+            <?php
+            }
+            ?>
+        </div>
+        <div id="captchaCheck" class="nkMarginBottom15">
+            <label class="nkLabelSpacing" for="code"><?php echo TYPESECCODE; ?></label>&nbsp;:&nbsp;
+                <input class="nkInput" type="text" id="code" name="codeConfirm" size="7" maxlength="5" />
+        </div>
+    </div>
+    <?php
+    return($random_code);
 }
 
 function crypt_captcha($code){
